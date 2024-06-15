69f0b8c16b5a94f94aa134983459fc942f3bd916
Slava <v4slava.guk@gmail.com>
2024-06-15 18:56:00 +0300

    add action_config second try

---
+++ src/logic/pages/menu/action_config.py
@@ -0,0 +1,38 @@
+from src.logic.pages.actions import Action
+
+from .configurationBox import ConfigurationBox
+from .menuBox import MenuBox
+
+page_boxs = ConfigurationBox | MenuBox
+
+action_config = {
+    'menu': {
+        'escape': Action.QUIT,
+        'return': Action.SHOW_CONF
+    },
+    'game_conf': {
+        'escape': Action.SHOW_MENU,
+        'return': Action.GO_TO_PLAY
+    }
+}
+
+
+def choose_box_action(key: str, state: page_boxs) -> Action | None:
+    match state:
+        case MenuBox():
+            box_key = 'menu'
+
+        case ConfigurationBox():
+            box_key = 'game_conf'
+
+        case _:
+            box_key = None
+
+    if box_key:
+        return action_config[box_key].get(key)
+
+
+def choose_alt_action(key: str) -> Action | None:
+    match key:
+        case key if key and len(key) == 1:
+            return Action.INPUT
