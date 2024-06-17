972b7e4540958bb53830d1be545faab3b0d3b46e
Slava <v4slava.guk@gmail.com>
2024-06-17 18:49:42 +0300

    new

--- src/logic/snakeApplication.py
+++ src/logic/snakeApplication.py
@@ -1,20 +1,20 @@
-from .logic.page import pages
-from .logic.playerHandle import PlayerHandle
+from .pages_collector import all_pages
+from .playerHandle import PlayerHandle
 
-from .logic.pages.page import Page
+from src.logic.pages.page import Page
 
-from .interface.infrastructure import Infrastructure
+from ..interface.infrastructure import Infrastructure
 
 
 class SnakeApplication:
-    def __init__(self, start_page: str = 'menu', player: PlayerHandle = PlayerHandle()) -> None:
-        self.infrastructure = Infrastructure()
+    def __init__(self, infrastructure: Infrastructure, start_page: str = 'menu') -> None:
         self.page_name = start_page
-        self.player = player
+        self.player = PlayerHandle()
+        self.infrastructure = infrastructure
 
     def launch(self) -> None:
         while True:
-            page_class = pages[self.page_name]
+            page_class = all_pages[self.page_name]
             page = page_class(self.infrastructure)
             self.launch_page(page)
 
@@ -32,8 +32,6 @@ class SnakeApplication:
     def get_page_player(self, page: Page) -> PlayerHandle | None:
         if self.page_name == 'menu':
             return page.player
-        else:
-            return
 
     def set_page_player(self, page: Page) -> None:
         if hasattr(page, 'player') and self.player:
