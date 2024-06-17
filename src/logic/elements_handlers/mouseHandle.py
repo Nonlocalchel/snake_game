004677373e48c64f2807edcf6aca78aa162b5c80
Slava <v4slava.guk@gmail.com>
2024-06-18 00:25:55 +0300

    fix mouse-pre-handle

--- src/logic/elements_handlers/mouseHandle.py
+++ src/logic/elements_handlers/mouseHandle.py
@@ -1,6 +1,6 @@
-from src.interface.infrastructure import Infrastructure
-
 from src.logic.app_elements.interface_elements.button import Button
+
+from src.interface.infrastructure import Infrastructure
 
 
 class MouseHandle:
@@ -8,60 +8,66 @@ class MouseHandle:
         self.infrastructure = infrastructure
         self.handle_element = None
 
+    @property
+    def state(self):
+        return self.handle_element.state
+
+    @state.setter
+    def state(self, new_state):
+        if new_state == self.state:
+            return
+
+        if new_state:
+            self.handle_state(new_state)
+
+        self.handle_element.state = new_state
+
     def handle(self, element: Button, elem_position: tuple[float, float]) -> None:
         self.handle_element = element
 
-        self.handle_pos(elem_position)
+        mouse_on_element = self.infrastructure.check_mouse(element.text, elem_position)
+        if mouse_on_element:
+            self.handle_mouse_over()
+        else:
+            self.handle_mouse_out()
+            self.state = None
+
         if element.is_hover:
             self.handle_click()
 
-        self.handle_state()
-
-    def handle_pos(self, position: tuple[float, float]) -> None:
-        element = self.handle_element
-
-        hover = self.infrastructure.check_mouse(element.text, position)
-        if hover:
-            self.handle_mouse_over()
-        else:
-            element.state = None
-
     def handle_mouse_over(self) -> None:
-        element = self.handle_element
-
-        if element.is_hover:
+        if self.handle_element.is_hover:
             return
 
-        self.infrastructure.play_hover_sound()
-        element.state = 'hover'
+        self.state = 'mouse_over'
+
+    def handle_mouse_out(self) -> None:
+        if self.state in ['mouse_up', 'mouse_over']:
+            self.state = 'mouse_out'
 
     def handle_click(self) -> None:
-        element = self.handle_element
-
-        if element.state == 'click':
+        if self.state == 'click':
             self.handle_mouse_up()
 
         is_click = self.infrastructure.is_click()
         if is_click:
             self.handle_mouse_down()
 
-        mouse_up = element.state == 'mouse_down' and not is_click
+        mouse_up = self.state == 'mouse_down' and not is_click
         if mouse_up:
             self.handle_mouse_up()
 
     def handle_mouse_down(self) -> None:
-        self.handle_element.state = 'mouse_down'
+        self.state = 'mouse_down'
 
     def handle_mouse_up(self) -> None:
-        element = self.handle_element
-
-        if element.state == 'mouse_down':
-            element.state = 'click'
+        if self.state == 'mouse_down':
+            self.state = 'click'
             return
 
-        element.state = 'mouse_up'
+        self.state = 'mouse_up'
 
-    def handle_state(self):
-        if self.handle_element.state == 'hover':
-            print('heandle_hover')
-
+    def handle_state(self, state: str) -> None:
+        match state:
+            case 'mouse_over':
+                self.infrastructure.play_hover_sound()
